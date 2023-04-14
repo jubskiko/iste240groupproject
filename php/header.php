4149e90b815dc74bda650353afc9314949bdecfe
Austin-Clifton-RIT <ajc4409@rit.edu>
2023-04-14 14:20:03 -0400

    starter files

--- php/header.php
+++ php/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?php echo $title;?> </title>
-    <script src=" <?php echo $path;?> /js/script.js"></script>
+    <script src="<?php echo $path;?>/js/script.js"></script>
     <link rel="stylesheet" href="<?php echo $path;?>/styles/styles.css">
 </head>
 
