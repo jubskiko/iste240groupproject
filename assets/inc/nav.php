a0493e48085e43b1c90f9a64c7989087671b6adc
jubskiko <joshuashales@gmail.com>
2023-04-24 21:52:26 -0400

    Update Nav a little bit

--- assets/inc/nav.php
+++ assets/inc/nav.php
@@ -3,6 +3,7 @@
     <a href="<?php echo $path;?>./index.php"><img class="logo" src="<?php echo $path;?>./assets/img/150x150.png" alt="Team Logo Image"></a>
 
     <ol class="list">
+        <a href="<?php echo $path;?>./index.php"><li>Home</li></a>
         <a href="<?php echo $path;?>pages/gettingStarted.php"><li>Getting Started</li></a>
         <a href="<?php echo $path;?>pages/commands.php"><li>Commands</li></a>
         <a href="<?php echo $path;?>pages/userManagement.php"><li>User Management</li></a>
@@ -12,6 +13,7 @@
         <a href="<?php echo $path;?>pages/systemMaintenance.php"><li>System Maintenance</li></a>
         <a href="<?php echo $path;?>pages/shellScripting.php"><li>Shell Scripting</li></a>
         <a href="<?php echo $path;?>pages/advancedTopics.php"><li>Advanced Topics</li></a>
+        <a href="<?php echo $path;?>pages/quiz.php"><li>Quiz!</li></a>
     </ol>
 
 </nav>
