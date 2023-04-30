64de8afac68b510190faafa8ce9129020becd9d4
Austin-Clifton-RIT <ajc4409@rit.edu>
2023-04-30 17:07:25 -0400

    changed colors for hover

--- assets/inc/nav.php
+++ assets/inc/nav.php
@@ -27,16 +27,16 @@ function writeActiveClass($currentLink) {
     <a href="<?php echo $path;?>./index.php"><img class="logo" src="<?php echo $path;?>./assets/img/150x150.png" alt="Team Logo Image"></a>
 
     <ol class="list">
-        <li><a href="<?php echo $path;?>pages/gettingStarted.php" <?php  writeActiveClass('gettingStarted'); ?> >Getting Started</a></li>
-        <li><a href="<?php echo $path;?>pages/commands.php" <?php  writeActiveClass('commands'); ?> >Commands</a></li>
-        <li><a href="<?php echo $path;?>pages/userManagement.php" <?php  writeActiveClass('userManagement'); ?> >User Management</a></li>
-        <li><a href="<?php echo $path;?>pages/fileManagement.php" <?php  writeActiveClass('fileManagement'); ?> >File Management</a></li>
-        <li><a href="<?php echo $path;?>pages/processManagement.php" <?php  writeActiveClass('processManagement'); ?> >Process Management</a></li>
-        <li><a href="<?php echo $path;?>pages/networking.php" <?php  writeActiveClass('networking'); ?> >Networking</a></li>
-        <li><a href="<?php echo $path;?>pages/systemMaintenance.php" <?php  writeActiveClass('systemMaintenance'); ?> >System Maintenance</a></li>
-        <li><a href="<?php echo $path;?>pages/shellScripting.php" <?php  writeActiveClass('shellScripting'); ?> >Shell Scripting</a></li>
-        <li><a href="<?php echo $path;?>pages/advancedTopics.php" <?php  writeActiveClass('advancedTopics'); ?> >Advanced Topics</a></li>
-        <li><a href="<?php echo $path;?>pages/quiz.php" <?php  writeActiveClass('quiz'); ?> >Quiz!</a></li>
+        <li><a class="beginner" href="<?php echo $path;?>pages/gettingStarted.php" <?php  writeActiveClass('gettingStarted'); ?> >Getting Started</a></li>
+        <li><a class="beginner" href="<?php echo $path;?>pages/commands.php" <?php  writeActiveClass('commands'); ?> >Commands</a></li>
+        <li><a class="beginner" href="<?php echo $path;?>pages/userManagement.php" <?php  writeActiveClass('userManagement'); ?> >User Management</a></li>
+        <li><a class="beginner" href="<?php echo $path;?>pages/fileManagement.php" <?php  writeActiveClass('fileManagement'); ?> >File Management</a></li>
+        <li><a class="intermeadiate" href="<?php echo $path;?>pages/processManagement.php" <?php  writeActiveClass('processManagement'); ?> >Process Management</a></li>
+        <li><a class="intermeadiate" href="<?php echo $path;?>pages/networking.php" <?php  writeActiveClass('networking'); ?> >Networking</a></li>
+        <li><a class="intermeadiate" href="<?php echo $path;?>pages/systemMaintenance.php" <?php  writeActiveClass('systemMaintenance'); ?> >System Maintenance</a></li>
+        <li><a class="advanced" href="<?php echo $path;?>pages/shellScripting.php" <?php  writeActiveClass('shellScripting'); ?> >Shell Scripting</a></li>
+        <li><a class="advanced" href="<?php echo $path;?>pages/advancedTopics.php" <?php  writeActiveClass('advancedTopics'); ?> >Advanced Topics</a></li>
+        <li><a class="advanced" href="<?php echo $path;?>pages/quiz.php" <?php  writeActiveClass('quiz'); ?> >Quiz!</a></li>
     </ol>
 
 </nav>
