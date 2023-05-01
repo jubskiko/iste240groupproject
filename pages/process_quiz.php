84ff04ed643a29ce6fe78c543c4b79d4e8d354c3
jubskiko <joshuashales@gmail.com>
2023-04-30 20:57:52 -0400

    Quiz

--- pages/process_quiz.php
+++ pages/process_quiz.php
@@ -1,31 +1,72 @@
 <?php
-    include("../dbcon.php");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $path = "../"; 
+    include($path . "dbcon.php");
 
-    function getQuizQuestions($conn) {
-        $sql = "SELECT * FROM easy";
-        $result = $conn->query($sql);
-        $questions = array();
+    $questions = array();
+    $total_score = 0;
 
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $questions[$row['id']] = $row;
+    foreach ($_POST as $key => $value) {
+        if (strpos($key, 'answer_') === 0) {
+            $question_id = substr($key, strlen('answer_'));
+            $user_answer = $conn->real_escape_string($value);
+
+            $sql = "SELECT * FROM easy WHERE id = $question_id LIMIT 1";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $correct_answer = strtoupper($row['correct_answer']);
+                if ($user_answer == $correct_answer) {
+                    $total_score++;
+                }
+                $questions[] = array(
+                    'question' => $row['question'],
+                    'user_answer' => $user_answer,
+                    'correct_answer' => $correct_answer
+                );
             }
         }
-        return $questions;
     }
 
-    if (isset($_POST["submit"])) {
-        $score = 0;
-        $questions = getQuizQuestions($conn);
+    $conn->close();
+    $score_percent = ($total_score / count($questions)) * 100;
+    $score_message = "You got $total_score out of " . count($questions) . " correct ($score_percent%).";
 
-        foreach ($questions as $question) {
-            $answer = isset($_POST["answer_" . $question["id"]]) ? $_POST["answer_" . $question["id"]] : null;
-            if ($answer === $question["correct_answer"]) {
-                $score++;
-            }
-        }
-
-        echo "Your score: " . $score . "/" . count($questions);
-        $conn->close();
-    }
+    $title = "Quiz Results";
+    include($path . "assets/inc/header.php");
 ?>
+<body>
+    <div class="container">    
+    <?php include($path . "assets/inc/nav.php"); ?>
+        <main class="content">
+        <h1>Quiz Results</h1>
+        <hr>
+        <p><?php echo $score_message; ?></p>
+        <table>
+            <thead>
+                <tr>
+                    <th>Question</th>
+                    <th>Your Answer</th>
+                    <th>Correct Answer</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($questions as $index => $question): ?>
+                <tr>
+                    <td><?php echo ($index + 1) . ". " . $question['question']; ?></td>
+                    <td><?php echo $question['user_answer']; ?></td>
+                    <td><?php echo $question['correct_answer']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+        </main>
+    </div>
+<?php
+    include($path . "assets/inc/footer.php");
+} else {
+    header("Location: ../index.php");
+}
+?>
+
--- pages/quiz.php
+++ pages/quiz.php
@@ -1,46 +1,46 @@
 <?php
     $title = "Quiz";
     $path = "../"; 
-    include($path . "dbcon.php");
+    include $path . "dbcon.php";
 
-    function getQuizQuestions($conn) {
-        $sql = "SELECT * FROM easy ORDER BY RAND() LIMIT 5";
-        $result = $conn->query($sql);
-        $questions = array();
+    $sql = "SELECT *
+            FROM easy 
+            ORDER BY RAND() 
+            LIMIT 5";
+    $result = $conn->query($sql);
+    $questions = array();
 
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $questions[] = $row;
-            }
+    // var_dump($result);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $questions[] = $row;
         }
-        return $questions;
     }
 
-    $questions = getQuizQuestions($conn);
-
-    include($path . "assets/inc/header.php");
-    $conn->close();
+    include $path . "assets/inc/header.php";
 ?>
+
 <body>
     <div class="container">    
-    <?php include($path . "assets/inc/nav.php"); ?>
+        <?php include $path . "assets/inc/nav.php"; ?>
         <main class="content">
-        <h1><?php echo($title)?></h1>
-        <hr>
-        <form id="quizForm" action="process_quiz.php" method="post">
-            <?php foreach ($questions as $index => $question): ?>
-            <p>
-            <?php echo ($index + 1) . ". " . $question["question"]; ?>
-            </p>
-            <?php foreach (range('A', 'D') as $choice): ?>
-            <input type="radio" name="answer_<?php echo $question["id"]; ?>" value="<?php echo $choice; ?>" required>
-            <?php echo $choice . ": " . $question["choice_" . strtolower($choice)]; ?><br>
-            <?php endforeach; ?>
-            <?php endforeach; ?>
-            <br>
-            <input type="submit" name="submit" value="Submit Quiz">
-        </form>
+            <h1><?php echo $title ?></h1>
+            <hr>
+            <form id="quizForm" method="post" action="process_quiz.php">
+                <?php foreach ($questions as $index => $question): ?>
+                <p>
+                    <?php echo ($index + 1) . ". " . $question["question"]; ?>
+                </p>
+                <?php foreach (range('A', 'D') as $choice): ?>
+                <input type="radio" name="answer_<?php echo $question["id"]; ?>" value="<?php echo $choice; ?>" required>
+                <?php echo $choice . ": " . $question["choice_" . strtolower($choice)]; ?><br>
+                <?php endforeach; ?>
+                <br>
+                <?php endforeach; ?>
+                <input type="submit" name="submit" value="Submit Quiz">
+            </form>
         </main>
     </div>
-</body>
-<?php include($path . "assets/inc/footer.php"); ?>
+</body>   
+<?php include $path . "assets/inc/footer.php"; ?>
